5bfe96cb7b103d5cd44c3efaf880a9cc6a49b548
Godspeed715 <chimzagamdaniel@gmail.com>
2026-03-22 18:15:08 +0100

    modified settings.py
    changed debug from true to false

--- web_security_guide/web_security_guide/settings.py
+++ web_security_guide/web_security_guide/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ct!-%^plcf44_(8%s#*w#q_4*$0(&be1m!cz1hbu82d6p*cx2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
