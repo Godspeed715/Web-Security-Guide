59eb1360d4d51a5b8160212c036a5091f0ac6a76
Godspeed715 <chimzagamdaniel@gmail.com>
2026-03-23 15:39:02 +0100

    moodified build.sh and added data.json
    to load data.json fixture after migrations.
    Updated settings.py to set ALLOWED_HOSTS for Render deployment.
    Deleted Procfile and build.sh from the root directory as they are no longer needed.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn web_security_guide.wsgi
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-pip install -r requirements.txt
-
-python web_security_guide/manage.py collectstatic --no-input
-python web_security_guide/manage.py migrate
