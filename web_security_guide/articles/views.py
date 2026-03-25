cfd373b74c0efd0b02b10335914f0128ddf799a0
Godspeed715 <chimzagamdaniel@gmail.com>
2026-03-25 08:46:33 +0100

    modified:  articles/views.py
    added login_required decorator to views in articles/views.py to restrict access to authenticated users only.

--- web_security_guide/articles/views.py
+++ web_security_guide/articles/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import OwaspTop10
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def owasp_list(request):
     """Redirect to first article"""
     return redirect('owasp', page_no=1)
 
+@login_required
 def owasp(request, page_no):
     page_count = OwaspTop10.objects.all().count()
     prev_id = page_no - 1 if page_no > 0 else None
