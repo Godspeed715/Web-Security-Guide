7429d26cdc4784da6231acfa42213d09d94b54e7
Godspeed715 <chimzagamdaniel@gmail.com>
2026-03-25 08:57:47 +0100

    modified:   home/views.py
    added user registration functionality to the views.py file. This includes importing the UserCreationForm, creating a signup view that handles both GET and POST requests, and rendering the appropriate templates for the home and signup pages.

--- web_security_guide/home/views.py
+++ web_security_guide/home/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('home')
+    
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/login.html', {'form': form})
